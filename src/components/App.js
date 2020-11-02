765fa176e8bf618e2400d1756f4a3010cfd79ae4
NishantBadlani <nishant.03badlani@gmail.com>
2020-11-02 16:43:34 +0530

    Changes4

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
-import React, {useState, useRef} from "react";
+import React, {useState, useRef, useEffect} from "react";
+import Task from "./task";
 import "./../styles/App.css";
 
 function App() {
   const [tasks, setTasks] = useState([]);
   const [inputValue, setInputValue] = useState("");
-  const [showEdit, setShowEdit] = useState(false);
-  const [editText, setEditText] = useState("");
+
   const taskId = useRef(0);
-  const editId = useRef(null);
 
   const handleChange = (event) => {
     setInputValue(event.target.value);
@@ -17,7 +16,6 @@ function App() {
     const updatedTasks = [...tasks];
     updatedTasks.push({id: taskId.current, text: inputValue});
     taskId.current = taskId.current + 1;
-    console.log(taskId.current);
     setInputValue("");
     setTasks(updatedTasks);
   };
@@ -28,53 +26,23 @@ function App() {
     setTasks(filteredTasks);
   };
 
-  const handleEdit = (id) => {
-    setShowEdit(true);
-    editId.current = id;
-  };
-
-  const handleTextChange = (event) => {
-    setEditText(event.target.value);
-  };
-
-  const saveChangedText = () => {
+  const saveChangedText = (id, newtext) => {
     const taskCopy = [...tasks];
     taskCopy.forEach((task) => {
-      if (task.id === editId.current) {
-        task.text = editText;
+      if (task.id === id) {
+        task.text = newtext;
       }
     });
     setTasks(taskCopy);
-    setShowEdit(false);
-    setEditText("");
   };
 
   return (
     <div id="main">
       <ul>
-        {tasks.map((task) => {
-          return (
-            <li className="list" key={task.id}>
-              {task.text}
-              <button className="edit" onClick={() => handleEdit(task.id)}>
-                Edit
-              </button>
-              <button className="delete" onClick={() => handleDelete(task.id)}>
-                Delete
-              </button>
-            </li>
-          );
-        })}
+        {tasks.map((task) => (
+          <Task key={task.id} task={task} saveChangedText={saveChangedText} handleDelete={handleDelete} />
+        ))}
       </ul>
-
-      {showEdit ? (
-        <div>
-          <input type="texarea" className="editTask" onChange={handleTextChange}></input>
-          <button className="saveTask" disabled={!editText} onClick={saveChangedText}>
-            Save
-          </button>
-        </div>
-      ) : null}
 
       <input id="task" type="textarea" value={inputValue} onChange={handleChange}></input>
       <button id="btn" onClick={handleClick} disabled={!inputValue}>
---
+++ src/components/task.js
@@ -0,0 +1,42 @@
+import React, {useState} from "react";
+
+export default function Task(props) {
+  const {task, saveChangedText, handleDelete} = props;
+
+  const [showEdit, setShowEdit] = useState(false);
+  const [editText, setEditText] = useState("");
+
+  const handleEdit = (id) => {
+    setShowEdit(!showEdit);
+  };
+
+  const handleTextChange = (event) => {
+    setEditText(event.target.value);
+  };
+
+  const saveNewText = (id) => {
+    saveChangedText(id, editText);
+    setShowEdit(false);
+    setEditText("");
+  };
+
+  return (
+    <li className="list" key={task.id}>
+      {task.text}
+      <button className="edit" onClick={() => handleEdit(task.id)}>
+        Edit
+      </button>
+      <button className="delete" onClick={() => handleDelete(task.id)}>
+        Delete
+      </button>
+      {showEdit ? (
+        <div>
+          <input type="texarea" className="editTask" onChange={handleTextChange}></input>
+          <button className="saveTask" disabled={!editText} onClick={() => saveNewText(task.id)}>
+            Save
+          </button>
+        </div>
+      ) : null}
+    </li>
+  );
+}
